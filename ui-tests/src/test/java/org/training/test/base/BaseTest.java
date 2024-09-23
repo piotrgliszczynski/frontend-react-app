dc94700b3ee82cbc118ce9c73f5ea83f1afd2db3
Piotr Gliszczyński <pgliszczynski@adp.com>
2024-09-23 18:42:11 +0200

    test/#2 - add screenshotting capability

--- ui-tests/src/test/java/org/training/test/base/BaseTest.java
+++ ui-tests/src/test/java/org/training/test/base/BaseTest.java
@@ -3,11 +3,14 @@ package org.training.test.base;
 import lombok.Getter;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
 import org.training.driver.factory.DriverFactory;
 import org.training.driver.type.DriverType;
+import org.training.test.config.ScreenShotter;
 
 @Getter
+@ExtendWith(ScreenShotter.class)
 public abstract class BaseTest {
 
   private WebDriver driver;
@@ -16,12 +19,18 @@ public abstract class BaseTest {
   void startDriver() {
     driver = DriverFactory.getManager(DriverType.CHROME).createDriver();
     prepareSteps();
+    prepareScreenshotter();
+  }
+
+  public void prepareScreenshotter() {
+    ScreenShotter.setDriver(getDriver());
   }
 
   public abstract void prepareSteps();
 
   @AfterEach
   void quitDriver() {
+
     driver.quit();
   }
 }
---
+++ ui-tests/src/test/java/org/training/test/config/ScreenShotter.java
@@ -0,0 +1,2 @@
+package org.training.test.config;public class Screenshotter {
+}
