88f8095e903efc21c0ad4759458875844941cfb2
Piotr Gliszczyński <pgliszczynski@adp.com>
2024-09-25 13:15:40 +0200

    feat/#55 - add possibility to input data into forms

--- react/src/components/AddUpdateForm.jsx
+++ react/src/components/AddUpdateForm.jsx
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useCustomer } from "./hooks/CustomerContext";
 
 const AddUpdateForm = () => {
 
   const { customer, emptyCustomer } = useCustomer();
+  const [customerData, setCustomerData] = useState(customer);
 
   const setTitle = () => {
-    return customer.id !== emptyCustomer.id ? 'Update' : 'Add'
+    return customerData.id !== emptyCustomer.id ? 'Update' : 'Add'
   }
+
+  const onType = (field, event) => {
+    console.log(field, event.target.value);
+    setCustomerData(
+      {
+        ...customerData,
+        [field]: event.target.value
+      }
+    )
+  }
+
+  useEffect(() => {
+    setCustomerData(customer)
+  }, [customer]);
 
   return (
     <>
       <h2 id="add-update-form-title">{setTitle()}</h2>
       <div>
         <label htmlFor="name">Name:</label>
-        <input type="text" id="name" placeholder="Customer Name" value={customer.name}></input>
+        <input type="text" id="name" onChange={(event) => onType('name', event)}
+          placeholder="Customer Name"
+          value={customerData.name}></input>
       </div>
       <div>
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" placeholder="name@company.com" value={customer.email}></input>
+        <input type="email" id="email" onChange={(event) => onType('email', event)}
+          placeholder="name@company.com"
+          value={customerData.email}></input>
       </div>
       <div>
         <label htmlFor="password">Pass:</label>
-        <input type="text" id="password" placeholder="password" value={customer.password}></input>
+        <input type="text" id="password" onChange={(event) => onType('password', event)}
+          placeholder="password"
+          value={customerData.password}></input>
       </div>
       <div>
         <button id="btn-delete">Delete</button>
