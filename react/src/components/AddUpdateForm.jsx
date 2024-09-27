b4c8ab6f1d350ebb14fb51b32d60b68e80578888
Piotr Gliszczyński <pgliszczynski@adp.com>
2024-09-27 14:44:32 +0200

    refactor/#100 - move and error message placing

--- react/src/components/AddUpdateForm.jsx
+++ react/src/components/AddUpdateForm.jsx
@@ -121,11 +121,11 @@ const AddUpdateForm = () => {
         <input type="text" id="password" onChange={(event) => onType('password', event)}
           placeholder="password"
           value={customerData.password}></input>
-        <div id="error-message">{errorMessage}</div>
         <div className="crud-buttons">
           <button id="btn-delete" onClick={onDelete}>Delete</button>
           <button id="btn-save" onClick={onSave}>Save</button>
           <button id="btn-cancel" onClick={onCancel}>Cancel</button>
+          <div id="error-message">{errorMessage}</div>
         </div>
       </div>
     </div >
