273d9f051b01463cf8fca0519ed5ad1950f9d962
Piotr Gliszczyński <pgliszczynski@adp.com>
2024-10-02 14:19:19 +0200

    feat/#123 - add auth context for RequireAuth component

--- react/src/components/RequireAuth.jsx
+++ react/src/components/RequireAuth.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
+import { useAuth } from './hooks/AuthContext';
 
 const RequireAuth = ({ children }) => {
   const location = useLocation();
-  const auth = {};
+  const auth = useAuth();
 
   if (!auth?.user) {
     return (
